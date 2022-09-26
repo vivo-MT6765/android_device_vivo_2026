09ffa02630a5a84683e6e913f1ae47983c1a41cd
Sushrut1101 <guptasushrut@gmail.com>
2022-09-26 16:16:29 +0530

    dandelion: Build the Snap Camera
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- device.mk
+++ device.mk
@@ -44,6 +44,10 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_PACKAGES += \
     audio.a2dp.default
 
+# Camera
+PRODUCT_PACKAGES += \
+    Snap
+
 # DT2W
 PRODUCT_PACKAGES += \
     DT2W-Service-Dandelion
