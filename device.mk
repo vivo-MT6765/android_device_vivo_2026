OtakuLinux/android_device_xiaomi_dandelion

dandelion: Add Mediatek IMS permissions
Signed-off-by: SparXFusion <s2234nadar@gmail.com>
Signed-off-by: ImFatF1sh <zijianjiao2017@gmail.com>
Change-Id: Id8535d388fba69e746719c059a891b5e905b17d0

device.mk
@@ -88,6 +88,9 @@ PRODUCT_BOOT_JARS += \
     mediatek-telephony-base \
     mediatek-telephony-common
 
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/permissions/privapp-permissions-mediatek.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-mediatek.xml
+
 # Screen density
 PRODUCT_AAPT_CONFIG := xhdpi
 PRODUCT_AAPT_PREF_CONFIG := xhdpi

vivo-MT6765/android_device_vivo_2026

Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

device.mk
@@ -74,6 +74,9 @@ PRODUCT_BOOT_JARS += \
     mediatek-telephony-base \
     mediatek-telephony-common
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/permissions/privapp-permissions-mediatek.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-mediatek.xml
+
 # Light
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.dandelion