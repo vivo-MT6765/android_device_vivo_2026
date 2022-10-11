OtakuLinux/android_device_xiaomi_dandelion

dandelion: Introduce 'SafailNet'
Bypass safetynet even if we have permissive selinux

Change-Id: Ib44cdbd15b53af3a1768cb0912f2dbcc56a8d97e
Signed-off-by: SparXFusion <s2234nadar@gmail.com>
Signed-off-by: ImFatF1sh <zijianjiao2017@gmail.com>

device.mk
@@ -48,6 +48,7 @@ TARGET_SCREEN_WIDTH := 720
 PRODUCT_PACKAGES += \
     init.mt6762.rc \
     init.mt6765.rc \
+    init.safailnet.rc \
     fstab.mt6762 \
     fstab.mt6765
 

vivo-MT6765/android_device_vivo_2026

Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

device.mk
@@ -90,7 +90,8 @@ PRODUCT_PACKAGES += \
     fstab.mt6765 \
     fstab.mt6762 \
     init.mt6765.rc \
-    init.mt6762.rc
+    init.mt6762.rc \
+    init.safailnet.rc
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.mt6765:$(TARGET_COPY_OUT_RAMDISK)/fstab.mt6765 \