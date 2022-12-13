1a77fa97290d4033ca8d26880de06c8bc98ad90e
Emmanuel <alzadidine33@gmail.com>
2022-12-13 08:23:08 +0000

    2026: Some fixes
    Signed-off-by: Emmanuel <alzadidine33@gmail.com>

--- device.mk
+++ device.mk
@@ -25,6 +25,9 @@ $(call inherit-product, vendor/vivo/2026/2026-vendor.mk)
 # API
 PRODUCT_SHIPPING_API_LEVEL := 29
 
+# VNDK
+PRODUCT_EXTRA_VNDK_VERSIONS := 30
+
 # Dynamic Partition
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_BUILD_SUPER_PARTITION := false
@@ -42,7 +45,7 @@ PRODUCT_PACKAGES += \
     audio.a2dp.default
 
 PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PLATFORM_VNDK_VERSION)/etc/audio_policy_configuration.xml
+    $(DEVICE_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_EXTRA_VNDK_VERSIONS)/etc/audio_policy_configuration.xml
 
 # GcamGo
 PRODUCT_PACKAGES += \
@@ -66,7 +69,7 @@ PRODUCT_PACKAGES += \
 
 # Light
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.2026
+    android.hardware.light@2.0-service.V2026
 
 # WIFI
 PRODUCT_PACKAGES += \
